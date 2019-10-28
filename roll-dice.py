c1db304b772805ab692c73f8911b1c6b30b59f14
SoumithKumar <soumithkumar5@gmail.com>
2019-10-28 23:39:27 +0100

    Delete roll-dice.py

--- roll-dice.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def dice():
-    ans = "yes";
-    while ans == "yes":
-        print(random.randrange(6))
-        ans = input("want to roll the dice again?, if so type 'yes': ")
-
-dice()
